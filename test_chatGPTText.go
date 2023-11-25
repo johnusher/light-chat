e8547d7b653f8b3072512b24daf21ba224b47c48
John Usher <yesjhon@gmail.com>
2023-11-25 21:31:28 +0100

    example response

--- test_chatGPTText.go
+++ test_chatGPTText.go
@@ -93,4 +93,22 @@ func main() {
 
 	fmt.Println(content)
 
+	f, err := os.Create("response.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.WriteString(content)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
